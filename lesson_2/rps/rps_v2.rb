46c54b3fc3b4033f2740f5906cf1cb68664ce1b4
Marek Martin <Marekwade1519@gamil.com>
2023-11-29 18:39:23 +0000

    intro and outro message

--- lesson_2/rps/rps_v2.rb
+++ lesson_2/rps/rps_v2.rb
@@ -6,6 +6,14 @@ class RPSGame
     @computer = Player.new
   end
 
+  def display_welcome_message
+    puts "Welcome to Rock, Paper, Scissors!"
+  end 
+  
+  def display_goodbye_message
+    puts "Thanks for playing Rock, Paper, Scissor. Good bye!"
+  end 
+  
   def play
     display_welcome_message
     human.choose
